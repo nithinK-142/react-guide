38fef8ac91822f1ba05952f9910ffea138314528
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-27 20:04:35 +0530

    feat: basic router template.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,12 @@ const navItems = [
   { to: "/", text: "Home" },
   { to: "/about", text: "About" },
   { to: "/contact", text: "Contact" },
-  { to: "/github", text: "Github" },
 ];
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 shadow">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+      <nav className="bg-stone-700 border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
           <Link to="/" className="flex items-center">
             <img
@@ -22,7 +21,7 @@ export default function Header() {
           <div className="flex items-center lg:order-2">
             <Link
               to="#"
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="text-white hover:bg-gray-50 hover:text-black focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Log in
             </Link>
@@ -41,7 +40,7 @@ export default function Header() {
                     to={item.to}
                     className={({ isActive }) =>
                       `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
+                        isActive ? "text-orange-700" : "text-white"
                       } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                     }
                   >
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import { Layout } from "./layout.tsx";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
-import Github from "./pages/Github.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +18,6 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="github" element={<Github />} />
     </Route>
   )
 );
