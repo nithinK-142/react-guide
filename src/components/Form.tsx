05bb1416ca4307d835939c2f0ea62dd92e0625da
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-15 09:09:35 +0530

    feat: validation using yup and react-hook-form.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,66 +1,83 @@
-import { IUser, validateUser } from "@/validations/user-validation";
-import { useState } from "react";
+import { UserSchemeType, userSchema } from "@/validations/user-validation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Form = () => {
-  const [result, setResult] = useState<IUser | null>(null);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
+  });
 
-  const createUser: React.FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-
-    const userData = {
-      name: formData.get("name") as string,
-      email: formData.get("email") as string,
-      password: formData.get("password") as string,
-    };
-
-    const validData = await validateUser(userData);
-    if (!validData) {
-      setResult(null);
-      return <h2>Invalid Details!</h2>;
-    } else {
-      setResult(validData);
-    }
+  const onSubmit = (data: UserSchemeType) => {
+    console.log(data);
+    alert("Form Submitted!");
   };
+
   return (
     <div className="flex flex-col items-center w-full">
-      <form onSubmit={createUser} className="min-w-[28rem]">
-        <div className="mb-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="min-w-[28rem]">
+        <div className="relative mb-5">
           <label htmlFor="text" className="label">
             Name
           </label>
-          <input type="text" name="name" id="text" className="input" />
+          <input type="text" className="input" {...register("name")} />
+          <span className="absolute right-0 text-sm text-red-600">
+            {" "}
+            {errors.name?.message}{" "}
+          </span>
         </div>
-        <div className="mb-5">
+        <div className="relative mb-5">
           <label htmlFor="email" className="label">
             Email
           </label>
-          <input type="email" name="email" id="email" className="input" />
+          <input
+            type="text"
+            id="email"
+            className="input"
+            {...register("email")}
+          />
+          <span className="absolute right-0 text-sm text-red-600">
+            {" "}
+            {errors.email?.message}{" "}
+          </span>
         </div>
-        <div className="mb-5">
+        <div className="relative mb-5">
           <label htmlFor="password" className="label">
             Password
           </label>
           <input
             type="password"
-            name="password"
             id="password"
             className="input"
+            {...register("password")}
           />
+          <span className="absolute right-0 text-sm text-red-600">
+            {" "}
+            {errors.password?.message}{" "}
+          </span>
+        </div>
+        <div className="relative mb-5">
+          <label htmlFor="repeatPassword" className="label">
+            Repeat password
+          </label>
+          <input
+            type="password"
+            id="repeatPassword"
+            className="input"
+            {...register("repeatPassword")}
+          />
+          <span className="absolute right-0 text-sm text-red-600">
+            {" "}
+            {errors.repeatPassword?.message}{" "}
+          </span>
         </div>
         <button type="submit" className="btn">
           Validate
         </button>
       </form>
-      <div className="min-w-[28rem] text-center pt-10">
-        {result && (
-          <>
-            <p>{result.name}</p>
-            <p>{result.email}</p>
-            <p>{result.password}</p>
-          </>
-        )}
-      </div>
     </div>
   );
 };
--- src/validations/user-validation.ts
+++ src/validations/user-validation.ts
@@ -1,22 +1,17 @@
-import { object, string, ValidationError } from "yup";
+import { object, string, ref } from "yup";
 
 export const userSchema = object().shape({
   name: string().min(4).required(),
   email: string().email().required(),
   password: string().min(4).max(10).required(),
+  repeatPassword: string()
+    .required()
+    .oneOf([ref("password")], "Passwords must match"),
 });
 
-export interface IUser {
+export type UserSchemeType = {
   name: string;
   email: string;
   password: string;
-}
-
-export const validateUser = async (userData: IUser) => {
-  try {
-    const isValid = await userSchema.validate(userData);
-    return isValid;
-  } catch (error) {
-    if (error instanceof ValidationError) alert(error.message);
-  }
+  repeatPassword: string;
 };
