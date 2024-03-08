ae2344fcabf19a119a5804d9e0bcee748a3af38d
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-08 13:01:41 +0530

    update: default port 5000.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/react-demo",
   plugins: [react()],
+  server: {
+    port: 5000,
+  },
   resolve: {
     alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`
+      "@/": `${path.resolve(__dirname, "src")}/`,
     },
   },
-})
+});
