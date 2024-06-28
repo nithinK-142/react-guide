61d9b1a01314aaec2aea1303314dbb55928eb2b8
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-28 21:40:31 +0530

    add: todo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,81 @@
-import BranchList from "./components/BranchList";
+import { useState } from "react";
+
+type Todo = {
+  task: string;
+  done: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      task: "get milk",
+      done: true,
+    },
+    {
+      task: "buy bread",
+      done: false,
+    },
+  ]);
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (!newTodo) {
+      alert("Taskfield is empty!");
+      return;
+    }
+    setTodos((prev) => [...prev, { task: newTodo, done: false }]);
+    setNewTodo("");
+  };
+
+  const removeTodo = (task: string) => {
+    setTodos((prev) => prev.filter((todo) => todo.task !== task));
+  };
+
   return (
     <main className="flex flex-col items-center text-center">
-      <h1 className="text-xl font-semibold">React Guide</h1>
-      <BranchList />
+      <h1 className="my-4 text-xl font-semibold">Task Manager</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo();
+        }}
+      >
+        <div className="flex space-x-4">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="px-2 py-1 text-black rounded-md"
+          />
+          <button className="px-4 py-1 text-black rounded-md bg-white/90">
+            Enter
+          </button>
+        </div>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-between my-2 space-x-2"
+          >
+            <span>{todo.task}</span>
+            <span>
+              {todo.done ? (
+                <span className="text-green-500">complete</span>
+              ) : (
+                <span className="text-red-500">incomplete</span>
+              )}
+            </span>
+            <button
+              onClick={() => removeTodo(todo.task)}
+              className="px-4 py-1 text-white rounded-md bg-red-500/90"
+            >
+              delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
