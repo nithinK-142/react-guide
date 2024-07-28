c56ae60bbf49cdbd63953b5254b40263b08d87ae
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-28 13:40:18 +0530

    add: react-window.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import BranchList from "./components/BranchList";
+import { FixedSizeList as List, ListChildComponentProps } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { useEffect } from "react";
+
+interface RowProps extends ListChildComponentProps {
+  index: number;
+  style: React.CSSProperties;
+}
+
+const Row: React.FC<RowProps> = ({ index, style }) => {
+  useEffect(() => {
+    console.log("mounting", index);
+    return () => console.log("unmounting", index);
+  }, [index]);
+
+  return (
+    <div
+      className={
+        index % 2
+          ? "border p-2 text-center"
+          : "border p-2 text-center bg-stone-800"
+      }
+      style={style}
+    >
+      Row {index}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <main className="flex flex-col items-center text-center">
-      <h1 className="text-xl font-semibold">React Guide</h1>
-      <BranchList />
+    <main className="bg-[#212121] h-[40rem] w-[20rem] mx-auto rounded-md">
+      <AutoSizer>
+        {({ height, width }) => (
+          <List height={height} itemCount={1000} itemSize={35} width={width}>
+            {Row}
+          </List>
+        )}
+      </AutoSizer>
     </main>
   );
 }
+
 export default App;
