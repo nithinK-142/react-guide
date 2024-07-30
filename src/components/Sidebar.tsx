077ed1c8ea7a020b8c37c378120d3e1c4f6dfd50
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-30 12:21:18 +0530

    fix: handle undefined email.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,13 +5,16 @@ import Button from "./Button";
 export default function Sidebar() {
   const { isAuthenticated, isLoading, user, login, register, logout } =
     useKindeAuth();
+  console.log(user);
   return (
     <div className="col-[2_/_3] row-[2_/_3] bg-[#fffcf9] pt-[18px] px-[25px] pb-[28px] flex flex-col justify-between border-l border-l-[rgba(0,0,0,0.08)]">
       <AddTodoForm />
       <div className="mt-auto space-y-2">
         {isLoading ? null : isAuthenticated ? (
           <>
-            <p className="text-sm">Logged in as {user?.email}</p>
+            <p className="text-sm">
+              Logged in as {user?.email || user?.given_name}
+            </p>
 
             <Button
               buttonType="secondary"
